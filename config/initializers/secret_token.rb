e75712750ac8a9f4f5ef1410c6b001beb0d41827
Takeru <tsutomu774@gmail.com>
2010-09-10 21:28:28 +0200

    initial commit eflora

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eflora::Application.config.secret_token = 'f7b7508a2ce10431e7d12cc39f19d9a0e4033a407937f7d36bc65029341cb37788e917d2887b1e09b6266e2c9dd267a57cdf5f1656658e4231425081a19762c8'
